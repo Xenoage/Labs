f0e183ba31fb74095aa0d98063f5e2c235960114
Andreas Wenger <andi@xenoage.com>
2014-06-17 17:00:03 +0200

    Updated controller and binding

--- JavaFXSymbolsTest/src/com/xenoage/labs/javafxsymbolstest/MainPanelController.java
+++ JavaFXSymbolsTest/src/com/xenoage/labs/javafxsymbolstest/MainPanelController.java
@@ -1,12 +1,16 @@
 package com.xenoage.labs.javafxsymbolstest;
 
-import javax.swing.JOptionPane;
-
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+import javax.swing.JOptionPane;
 
 /**
  * Controller for the MainPanel.fxml
@@ -16,14 +20,19 @@ import javafx.scene.control.Slider;
 public class MainPanelController {
 	
 	@FXML private Slider sliderSymbols;
+	@FXML private TabPane tabPane;
 	@FXML private Canvas canvasPaths;
 	
 	private IntegerProperty symbolsCount = new SimpleIntegerProperty();
 	
 	
 	public void initialize() { 
+		//number of symbols
 		symbolsCount.bind(sliderSymbols.valueProperty());
-		symbolsCount.addListener(n -> JOptionPane.showMessageDialog(null, n));
+		sliderSymbols.valueProperty().addListener(n -> JOptionPane.showMessageDialog(null, n));
+		//tab changes
+		tabPane.getSelectionModel().selectedItemProperty().addListener(
+			(ov, oldVal, newVal) -> JOptionPane.showMessageDialog(null, newVal.getText()));
 	}
 	
 	
